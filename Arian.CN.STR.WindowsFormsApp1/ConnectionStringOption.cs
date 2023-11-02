6993e0ef16010158894f5724b34ee2aa372cbfaf
nader <nhashemi6@gmail.com>
2023-11-02 11:42:14 +0330

    Add project files.

---
+++ Arian.CN.STR.WindowsFormsApp1/ConnectionStringOption.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Linq;
+
+namespace PayamHannan.Arian.ORM
+{
+    public class ConnectionStringOption
+    {
+
+
+        public ConnectionStringOption()
+        {
+        }
+        public ConnectionStringOption(string connectionString)
+        {
+            var options = connectionString.Split(";".ToArray(), StringSplitOptions.RemoveEmptyEntries).ToDictionary(k => k.Split('=')[0], k => k.Split('=')[1]);
+
+            DataSource = options["Data Source"];
+            InitialCatalog = options["Initial Catalog"];
+            Password = options["Password"];
+            PersistSecurityInfo = Convert.ToBoolean(options.ContainsKey("Persist Security Info") ? options["Persist Security Info"] : "True");
+            UserID = options["User ID"];
+
+        }
+
+        public string DataSource { get; set; }
+        public string InitialCatalog { get; set; }
+        public string UserID { get; set; }
+        public string Password { get; set; }
+        public bool PersistSecurityInfo { get; set; }
+
+        public override string ToString()
+        => string.Format("Data Source={0};Initial Catalog={1};Persist Security Info={2};User ID={3};Password={4};", DataSource, InitialCatalog, PersistSecurityInfo ? "True" : "False", UserID, Password);
+
+    }
+}
---
+++ Arian.CN.STR.WindowsFormsApp1/Form1.cs
@@ -0,0 +1,31 @@
+﻿using PayamHannan.Arian.ORM;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Arian.CN.STR.WindowsFormsApp1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            var CN = new ConnectionStringOption();
+            CN.DataSource = ".";
+            CN.InitialCatalog = "Northwind";
+            CN.Password = "1234";
+            CN.UserID = "sa";
+            var STR = CN.ToString();
+        }
+    }
+}
